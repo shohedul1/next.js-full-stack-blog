825e6926e01957497ca8ed52de9430ed0a6a3f10
shohidul <shohidul@example.com>
2023-11-17 00:29:03 -0800

    shohidul2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,24 @@
-import { authOptions } from "@/lib/auth";
+// import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth"
 
+// const handler = NextAuth(authOptions)
+
+// export { handler as GET, handler as POST }
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/db";
+
+
+export const authOptions = {
+    adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      })
+  ],
+}
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+
