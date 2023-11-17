329188526f58f750df8430d34097340f4ff858cb
shohidul <shohidul@example.com>
2023-11-17 00:38:16 -0800

    shohidul4

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,27 @@
-// import { authOptions } from "@/lib/auth";
-import NextAuth from "next-auth"
+// import { authOptions } from "@/lib/auth"
+// import NextAuth from "next-auth"
 
 // const handler = NextAuth(authOptions)
 
 // export { handler as GET, handler as POST }
+import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/lib/db";
+import GitHubProvider from "next-auth/providers/github";
+
 
 
 const authOptions = {
-    adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    })
   ],
-}
-const handler = NextAuth(authOptions)
+};
 
-export { handler as GET, handler as POST }
-
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST}
