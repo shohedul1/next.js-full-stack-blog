75114f3db79e6b4991d7e20435bc80e431a1a549
shohidul <shohidul@example.com>
2023-11-17 01:33:48 -0800

    shohidul4

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,6 @@
-// import { authOptions } from "@/lib/auth"
-// import NextAuth from "next-auth"
+import { authOptions } from "@/lib/auth"
+import NextAuth from "next-auth"
 
-// const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
-// export { handler as GET, handler as POST }
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider from "next-auth/providers/github";
-
-
-
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    })
-  ],
-};
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
